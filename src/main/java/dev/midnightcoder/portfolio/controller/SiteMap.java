dc21cddc246567046bc8f020e5f001989cd319d9
Glabay <Java_Guru@live.com>
2026-06-03 22:00:44 -0300

    init commit

--- src/main/java/dev/midnightcoder/portfolio/controller/SiteMap.java
+++ src/main/java/dev/midnightcoder/portfolio/controller/SiteMap.java
@@ -1,9 +1,19 @@
 package dev.midnightcoder.portfolio.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author Glabay | Glabay-Studios
  * @project Portfolio
  * @social Discord: Glabay
  * @since 2026-06-02
-*/public class SiteMap {
+ */
+@Controller
+public class SiteMap {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
 }

4f498ad9b99b11c6f4327b16e71f532755507862
Glabay <Java_Guru@live.com>
2026-06-03 22:00:25 -0300

    Initial commit

---
+++ src/main/java/dev/midnightcoder/portfolio/controller/SiteMap.java
@@ -0,0 +1,9 @@
+package dev.midnightcoder.portfolio.controller;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project Portfolio
+ * @social Discord: Glabay
+ * @since 2026-06-02
+*/public class SiteMap {
+}
